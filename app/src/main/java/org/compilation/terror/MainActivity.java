f78dbd948d46fe906a2b53f398564bcec020b84e
505-NN-505 <nurnasrum@iut-dhaka.edu>
2022-10-18 01:06:44 +0600

    Initial commit

---
+++ app/src/main/java/org/compilation/terror/MainActivity.java
@@ -0,0 +1,16 @@
+package org.compilation.terror;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kyanogen.signatureview.SignatureView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
